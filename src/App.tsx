f27790fe8c9e7f34b98aed50846ea7dc64b430a1
Felipe Moraes Palomo <felipemoraespalomo@gmail.com>
2024-03-08 19:29:45 -0300

    BrowserRouter Initial Settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { GlobalCss } from './styles'
 import Header from './components/Header'
 import Banner from './components/Banner'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Banner />
+  }
+])
 
 function App() {
   return (
@@ -11,7 +18,7 @@ function App() {
       <div className="container">
         <Header />
       </div>
-      <Banner />
+      <RouterProvider router={rotas} />
     </>
   )
 }
