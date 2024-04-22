1d04bc8973270f10e2ed2b32d4a45990fb6c172b
Felipe Moraes Palomo <felipemoraespalomo@gmail.com>
2024-04-22 17:12:48 -0300

    Feat: finish gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,25 +1,125 @@
 import Section from '../Section'
-import zelda from '../../assets/images/zelda.png'
 
 import * as S from './styles'
 
-const Gallery = () => (
-  <Section title="Galeria" background="black">
-    <S.Items>
-      <S.Item>
-        <img src={zelda} alt="imagem do link" />
-      </S.Item>
-      <S.Item>
-        <img src={zelda} alt="imagem do link" />
-      </S.Item>
-      <S.Item>
-        <img src={zelda} alt="imagem do link" />
-      </S.Item>
-      <S.Item>
-        <img src={zelda} alt="imagem do link" />
-      </S.Item>
-    </S.Items>
-  </Section>
-)
+import spider from '../../assets/images/banner-homem-aranha.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
+import { useState } from 'react'
+
+interface GalleryItem {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: spider
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=iI7lmIWKpAUlkZAp'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+interface ModalState extends GalleryItem {
+  isVisible: boolean
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const [modal, setModal] = useState<ModalState>({
+    isVisible: false,
+    type: 'image',
+    url: ''
+  })
+
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  const closeModal = () => {
+    setModal({
+      isVisible: false,
+      type: 'image',
+      url: 'url'
+    })
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="black">
+        <S.Items>
+          {mock.map((media, index) => (
+            <S.Item
+              key={media.url}
+              onClick={() => {
+                setModal({
+                  isVisible: true,
+                  type: media.type,
+                  url: media.url
+                })
+              }}
+            >
+              <img
+                src={getMediaCover(media)}
+                alt={`Mídia ${index + 1} de ${name}`}
+              />
+              <S.Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a mídia"
+                />
+              </S.Action>
+            </S.Item>
+          ))}
+        </S.Items>
+      </Section>
+      <S.Modal className={modal.isVisible ? 'visivel' : ''}>
+        <S.ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img
+              src={fechar}
+              alt="ícone de fechar"
+              onClick={() => {
+                closeModal()
+              }}
+            />
+          </header>
+          {modal.type === 'image' ? (
+            <img src={modal.url} />
+          ) : (
+            <iframe frameBorder={0} src={modal.url} />
+          )}
+        </S.ModalContent>
+        <div
+          onClick={() => {
+            closeModal()
+          }}
+          className="overlay"
+        ></div>
+      </S.Modal>
+    </>
+  )
+}
 
 export default Gallery
