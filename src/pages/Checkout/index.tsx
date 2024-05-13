97fa6b67468f93910579ca010d5166c604dd49ca
Felipe Moraes Palomo <felipemoraespalomo@gmail.com>
2024-05-13 17:44:55 -0300

    feat: customize card payment section

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,54 +1,127 @@
+import { useState } from 'react'
 import Button from '../../components/Button'
 import Card from '../../components/Card'
 
+import boleto from '../../assets/images/boleto.png'
+import cartao from '../../assets/images/cartao.png'
+
 import * as S from './styles'
 
-const Checkout = () => (
-  <div className="container">
-    <Card title="Dados de cobrança">
-      <>
-        <S.Row>
-          <S.InputGroup>
-            <label htmlFor="fullName">Nome completo</label>
-            <input type="text" id="fullName" />
-          </S.InputGroup>
-          <S.InputGroup>
-            <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" />
-          </S.InputGroup>
-          <S.InputGroup>
-            <label htmlFor="cpf">CPF</label>
-            <input type="text" id="cpf" />
-          </S.InputGroup>
-        </S.Row>
-        <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
-        <S.Row>
-          <S.InputGroup>
-            <label htmlFor="deliveryEmail">E-mail</label>
-            <input type="email" id="deliveryEmail" />
-          </S.InputGroup>
-          <S.InputGroup>
-            <label htmlFor="confirmDeliveryEmail">Confirme o e-mail</label>
-            <input type="email" id="confirmDeliveryEmail" />
-          </S.InputGroup>
-        </S.Row>
-      </>
-    </Card>
-    <Card title="Pagamento">
-      <div>
-        <p>
-          Ao optar por essa forma de pagamento, é importante lembrar que a
-          confirmação pode levar até 3 dias úteis, devido aos prazos
-          estabelecidos pelas instituições financeiras. Portanto, a liberação do
-          código de ativação do jogo adquirido ocorrerá somente após a aprovação
-          do pagamento do boleto.
-        </p>
-      </div>
-    </Card>
-    <Button type="button" title="Clique aqui para finalizar a compra">
-      Finalizar compra
-    </Button>
-  </div>
-)
+const Checkout = () => {
+  const [payWithCard, setPayWithCard] = useState(false)
+
+  return (
+    <div className="container">
+      <Card title="Dados de cobrança">
+        <>
+          <S.Row>
+            <S.InputGroup>
+              <label htmlFor="fullName">Nome completo</label>
+              <input type="text" id="fullName" />
+            </S.InputGroup>
+            <S.InputGroup>
+              <label htmlFor="email">E-mail</label>
+              <input type="email" id="email" />
+            </S.InputGroup>
+            <S.InputGroup>
+              <label htmlFor="cpf">CPF</label>
+              <input type="text" id="cpf" />
+            </S.InputGroup>
+          </S.Row>
+          <h3 className="margin-top">Dados de entrega - conteúdo digital</h3>
+          <S.Row>
+            <S.InputGroup>
+              <label htmlFor="deliveryEmail">E-mail</label>
+              <input type="email" id="deliveryEmail" />
+            </S.InputGroup>
+            <S.InputGroup>
+              <label htmlFor="confirmDeliveryEmail">Confirme o e-mail</label>
+              <input type="email" id="confirmDeliveryEmail" />
+            </S.InputGroup>
+          </S.Row>
+        </>
+      </Card>
+      <Card title="Pagamento">
+        <>
+          <S.TabButton
+            isActive={!payWithCard}
+            onClick={() => setPayWithCard(false)}
+          >
+            <img src={boleto} alt="Boleto" />
+            Boleto bancário
+          </S.TabButton>
+          <S.TabButton
+            isActive={payWithCard}
+            onClick={() => setPayWithCard(true)}
+          >
+            <img src={cartao} alt="Cartão de crédito" />
+            Cartão de crédito
+          </S.TabButton>
+          <div className="margin-top">
+            {payWithCard ? (
+              <>
+                <S.Row>
+                  <S.InputGroup>
+                    <label htmlFor="cardOwner">Nome do titular do cartão</label>
+                    <input type="text" id="cardOwner" />
+                  </S.InputGroup>
+                  <S.InputGroup>
+                    <label htmlFor="cpfCardOwner">
+                      CPF do titular do cartão
+                    </label>
+                    <input type="text" id="cpfCardOwner" />
+                  </S.InputGroup>
+                </S.Row>
+                <S.Row marginTop="24px">
+                  <S.InputGroup>
+                    <label htmlFor="cardDisplayName">Nome no cartão</label>
+                    <input type="text" id="cardDisplayName" />
+                  </S.InputGroup>
+                  <S.InputGroup>
+                    <label htmlFor="cardNumber">Número no cartão</label>
+                    <input type="text" id="cardNumber" />
+                  </S.InputGroup>
+                  <S.InputGroup maxWidth="123px">
+                    <label htmlFor="expiresMonth">Mês do vencimento</label>
+                    <input type="text" id="expiresMonth" />
+                  </S.InputGroup>
+                  <S.InputGroup maxWidth="123px">
+                    <label htmlFor="expiresYear">Ano de vencimento</label>
+                    <input type="text" id="expiresYear" />
+                  </S.InputGroup>
+                  <S.InputGroup maxWidth="48px">
+                    <label htmlFor="cardCode">CVV</label>
+                    <input type="text" id="cardCode" />
+                  </S.InputGroup>
+                </S.Row>
+                <S.Row marginTop="24px">
+                  <S.InputGroup maxWidth="150px">
+                    <label htmlFor="installments">Parcelamento</label>
+                    <select name="" id="">
+                      <option>1x de R$200</option>
+                      <option>2x de R$100</option>
+                      <option>3x de R$200</option>
+                    </select>
+                  </S.InputGroup>
+                </S.Row>
+              </>
+            ) : (
+              <p>
+                Ao optar por essa forma de pagamento, é importante lembrar que a
+                confirmação pode levar até 3 dias úteis, devido aos prazos
+                estabelecidos pelas instituições financeiras. Portanto, a
+                liberação do código de ativação do jogo adquirido ocorrerá
+                somente após a aprovação do pagamento do boleto.
+              </p>
+            )}
+          </div>
+        </>
+      </Card>
+      <Button type="button" title="Clique aqui para finalizar a compra">
+        Finalizar compra
+      </Button>
+    </div>
+  )
+}
 
 export default Checkout
